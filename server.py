873f77be9d211bb2dbe743caa88f510d316b7088
Ismail  Mansuri <128184761+mansurii@users.noreply.github.com>
2026-03-15 01:01:56 +0000

    Configure static site with index.html as the starting page

--- server.py
+++
@@ -1,9 +0,0 @@
-import http.server
-import socketserver
-
-PORT = 5000
-Handler = http.server.SimpleHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Server running at http://localhost:{PORT}")
-    httpd.serve_forever()
